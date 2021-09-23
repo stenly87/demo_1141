2a97e4cde6589f84af2c9ac9ff10607e44d9e5f3
ppk <ppk_student@hotmail.com>
2021-09-23 13:46:21 +1000

    редактируем клиента

--- WpfApp2/DB/ClientExtension.cs
+++ WpfApp2/DB/ClientExtension.cs
@@ -8,6 +8,35 @@ namespace WpfApp2.DB
 {
     public partial class Client
     {
+        public string PhotoPathAbsolute 
+        {
+            get => Environment.CurrentDirectory + "\\" + PhotoPath;
+        }
+        public bool GenderMan 
+        {
+            get => Gender.Code == "м";
+            set
+            {
+                if (value)
+                {
+                    Gender = DBInstance.GetInstance().Gender.
+                        FirstOrDefault(code => code.Code == "м");
+                }
+            }
+        }
+        public bool GenderWoman
+        {
+            get => Gender.Code == "ж";
+            set
+            {
+                if (value)
+                {
+                    Gender = DBInstance.GetInstance().Gender.
+                        FirstOrDefault(code => code.Code == "ж");
+                }
+            }
+        }
+
         public DateTime? LastVisitDate 
         {
             get
--- WpfApp2/ViewModels/EditClientViewModel.cs
+++ WpfApp2/ViewModels/EditClientViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfApp2.DB;
 using WpfApp2.Tools;
 
@@ -12,14 +15,38 @@ namespace WpfApp2.ViewModels
     {
         public Client EditClient { get; set; }
 
-        public EditClientViewModel(Client editClient)
-        {
-            EditClient = editClient;
-        }
-
         public CustomCommand SelectImage { get; set; }
         public CustomCommand Save { get; set; }
 
+        public EditClientViewModel(Client editClient)
+        {
+            EditClient = editClient;
 
+            SelectImage = new CustomCommand(()=> 
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() != true)
+                    return;
+                if (!File.Exists(openFileDialog.FileName))
+                {
+                    MessageBox.Show("Файл не найден!");
+                    return;
+                }
+                if (new FileInfo(openFileDialog.FileName).Length > 2 * 1024 * 1024)
+                {
+                    MessageBox.Show("Размер файла превышает 2МБ!");
+                    return;
+                }
+                string destFileName = $"{Environment.CurrentDirectory}\\Клиенты\\" +
+                    $"{new FileInfo(openFileDialog.FileName).Name}";
+                if (File.Exists(destFileName))
+                    destFileName = $"{Environment.CurrentDirectory}\\Клиенты\\" +
+                    $"{editClient.ID}{new FileInfo(openFileDialog.FileName).Name}";
+                File.Copy(openFileDialog.FileName, destFileName, true);
+                editClient.PhotoPath = destFileName.
+                    Replace($"{Environment.CurrentDirectory}\\", "");
+                SignalChanged("PhotoPathAbsolute"); // не пашет!
+            });
+        }
     }
 }
